9321daeb0703b45eaff75435b6aa7372e94b31d3
Nelson Villarreal <devnelsonvillarreal@gmail.com>
2025-06-01 18:25:16 -0400

    docs(api-login): melhora a legibilidade do teste com comentários explicativos

--- cypress/e2e/api/02-lista-produtos.api.cy.js
+++ cypress/e2e/api/02-lista-produtos.api.cy.js
@@ -1,9 +1,9 @@
 describe('API - Listagem de Produtos', () => {
   it('Deve retornar a lista de produtos disponíveis', () => {
     cy.request('https://serverest.dev/produtos').then((res) => {
-      expect(res.status).to.eq(200);
-      expect(res.body.quantidade).to.be.greaterThan(0);
-      expect(res.body.produtos).to.be.an('array');
+      expect(res.status).to.eq(200); //verifica status 200
+      expect(res.body.quantidade).to.be.greaterThan(0); //verifica se tem produtos
+      expect(res.body.produtos).to.be.an('array'); //garante o campo produtos no cropo da resposta, o esperado de uma list
     });
   });
 });
